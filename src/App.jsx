1cb00bff8c093736ee15c8d27dcf6aa80b1e4339
MrWalt <mihaelkrstevski.2002@yahoo.com>
2024-06-08 08:32:35 +0200

    Fix context menu outside click bug and remove Dev Tools

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ export default function App() {
   return (
     <DarkModeProvider>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
+        {/* <ReactQueryDevtools initialIsOpen={false} /> */}
         <GlobalStyles />
         <BrowserRouter>
           <Routes>
